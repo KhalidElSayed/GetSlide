1ec1a107925c4d52f11046fe7a5a037a97d3bfbb
amir hayek <free4sale@gmail.com>
2013-03-07 10:18:57 +0200

    first import
    empty project

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GetSlide
+//
+//  Created by amir hayek on 3/7/13.
+//  Copyright (c) 2013 amir hayek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GetSlide
+//
+//  Created by amir hayek on 3/7/13.
+//  Copyright (c) 2013 amir hayek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
